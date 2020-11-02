da5e53f1b311879f49507669a54ff9418ced6e5a
陈测试 <1195733026@qq.com>
2020-11-02 09:10:11 +0800

    Update auto_driver.py
    优化驱动调用程序

--- auto_driver/auto_driver.py
+++ auto_driver/auto_driver.py
@@ -8,38 +8,37 @@
 @File : auto_driver.py
 @Project : AutoTestPlatform
 """
-from auto_driver.mobile.appium_driver import AppIumDriver
-from function.get_config import get
+from auto_driver import *
 from function.exception_action import ExceptionAction
 
 
 class Driver(object):
     def __init__(self, platform_name):
+        """
+        初始化驱动调用程序
+        :param platform_name: 平台类型：iOS,Android
+        """
+        self.platform_name = platform_name
+
+    def driver(self):
+        """
+        驱动调用
+        :return:
+        """
         try:
-            if "iOS" == platform_name or "Android" == platform_name:
-                self.platform_name = platform_name
-                self.remote_ip = get("app", "remote_ip")
-                self.remote_port = get("app", "remote_port")
-                self.device_name = get(platform_name, "device_name")
-                self.platform_version = get(platform_name, "platform_version")
-                self.app_package = get(platform_name, "app_package")
-                self.app_activity = get(platform_name, "app_activity")
-                self.mobile_driver = AppIumDriver(platform_name=self.platform_name,
-                                                  remote_ip=self.remote_ip,
-                                                  remote_port=self.remote_port,
-                                                  device_name=self.device_name,
-                                                  platform_version=self.platform_version,
-                                                  app_package=self.app_package,
-                                                  app_activity=self.app_activity).driver
-            elif "pc" == platform_name:
+            if "iOS" in self.platform_name or "Android" in self.platform_name:
+                self.app_driver = AppDriver(self.platform_name).mobile_driver
+                return self.app_driver
+            elif "pc" in self.platform_name:
                 pass
-            elif "web" == platform_name:
+            elif "web" in self.platform_name:
+                self.web_driver = WebChromeDriver().chrome_browser
+                return self.web_driver
+            elif "api" in self.platform_name:
                 pass
-            elif "api" == platform_name:
+            elif "shell" in self.platform_name:
                 pass
-            elif "shell" == platform_name:
-                pass
-            elif "sip" == platform_name:
+            elif "sip" in self.platform_name:
                 pass
         except Exception as e:
             ExceptionAction().log().error(e)
