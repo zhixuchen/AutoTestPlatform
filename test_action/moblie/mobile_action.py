9723b69bb34ae078dec637cac5c26b16a0070dc3
陈测试 <1195733026@qq.com>
2020-11-16 14:45:46 +0800

    Update mobile_action.py

--- test_action/moblie/mobile_action.py
+++ test_action/moblie/mobile_action.py
@@ -8,24 +8,24 @@
 @File : mobile_action.py
 @Project : AutoTestPlatform
 """
-from auto_driver.auto_driver import Driver
-from element.elements import Element
-from global_variable import *
-from function.check import CheckAssert
+from WebforAutoTestPlatform.auto_driver.auto_driver import Driver
+from WebforAutoTestPlatform.element.elements import Element
+from WebforAutoTestPlatform.global_variable import *
+from WebforAutoTestPlatform.function.check import CheckAssert
 
 import os
 
 
 class MobileAction(object):
-    def __init__(self):
+    def __init__(self,suite_id):
         """
         初始化
         """
         self.check = CheckAssert()
         self.action_plugins = {}
-        self.load_actions()
+        self.load_actions(suite_id)
 
-    def load_actions(self):
+    def load_actions(self,suite_id):
         """
         加载action的插件
         :return: 返回acton插件的类
@@ -34,19 +34,22 @@ class MobileAction(object):
             if not filename.endswith(".py") or filename.startswith("_"):
                 continue
             action_name = os.path.splitext(filename)[0]
-            action = __import__("static_file.mobile_plugins." + action_name, fromlist=[action_name])
-            action_class = action.get_plugin_class()
+            action = __import__("WebforAutoTestPlatform.static_file.mobile_plugins." + action_name,
+                                fromlist=[action_name])
+            action_class = action.get_plugin_class(suite_id)
             action_class_name = action_class.__name__
             self.action_plugins[action_class_name] = action_class
 
-    def driver(self, platform_name):
+    def driver(self):
         """
         获取驱动
         :param platform_name: 平台类型
         :return: 驱动
         """
-        self.driver = Driver(platform_name).driver()
-        return self.driver
+        global DRIVER
+        if DRIVER==None:
+            DRIVER= Driver().driver()
+        return DRIVER
 
     def element(self, driver):
         """
