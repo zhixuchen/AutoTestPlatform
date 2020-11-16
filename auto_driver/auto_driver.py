ddbe20d8a99a4ec8c62876f0aeee07b4b452c862
陈测试 <1195733026@qq.com>
2020-11-16 14:39:11 +0800

    Update auto_driver.py

--- auto_driver/auto_driver.py
+++ auto_driver/auto_driver.py
@@ -8,17 +8,18 @@
 @File : auto_driver.py
 @Project : AutoTestPlatform
 """
-from auto_driver import *
-from function.exception_action import ExceptionAction
+from WebforAutoTestPlatform.auto_driver import *
+from WebforAutoTestPlatform.function.exception_action import ExceptionAction
 
 
 class Driver(object):
-    def __init__(self, platform_name):
+    def __init__(self):
         """
         初始化驱动调用程序
         :param platform_name: 平台类型：iOS,Android
         """
-        self.platform_name = platform_name
+
+        self.platform = str(Config.objects.get(conf_key="platform"))
 
     def driver(self):
         """
@@ -26,19 +27,22 @@ class Driver(object):
         :return:
         """
         try:
-            if "iOS" in self.platform_name or "Android" in self.platform_name:
-                self.app_driver = AppDriver(self.platform_name).mobile_driver
+
+            if "iOS" in self.platform or "Android" in self.platform:
+
+                self.app_driver = AppDriver().mobile_driver
+                print("打印Driver的内容：", self.app_driver)
                 return self.app_driver
-            elif "pc" in self.platform_name:
+            elif "pc" in self.platform:
                 pass
-            elif "web" in self.platform_name:
+            elif "web" in self.platform:
                 self.web_driver = WebChromeDriver().chrome_browser
                 return self.web_driver
-            elif "api" in self.platform_name:
+            elif "api" in self.platform:
                 pass
-            elif "shell" in self.platform_name:
+            elif "shell" in self.platform:
                 pass
-            elif "sip" in self.platform_name:
+            elif "sip" in self.platform:
                 pass
         except Exception as e:
             ExceptionAction().log().error(e)
