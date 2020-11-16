3b87ea1e33437f3f5b763c01019e2ae7cadcaef0
陈测试 <1195733026@qq.com>
2020-11-16 14:44:39 +0800

    Update Login.py

--- static_file/mobile_plugins/Login.py
+++ static_file/mobile_plugins/Login.py
@@ -8,18 +8,18 @@
 @File : Login.py
 @Project : AutoTestPlatform
 """
-from function.exception_action import ExceptionAction
-from test_action.moblie.android import B
-from test_action.moblie.ios import I
-from test_action.moblie.mobile_action import MobileAction
-
+from WebforAutoTestPlatform.function.exception_action import ExceptionAction
+from WebforAutoTestPlatform.test_action.moblie.android import B
+from WebforAutoTestPlatform.test_action.moblie.ios import I
+from WebforAutoTestPlatform.test_action.moblie.mobile_action import MobileAction
+from Test_Platform.models import Config
 
 
 class Login(object):
-    def __init__(self, *args, **kwargs):
-        self.platform_name = args[0]
-        self.driver = MobileAction().driver(self.platform_name)
-        self.element = MobileAction().element(self.driver)
+    def __init__(self, **kwargs):
+        self.platform = str(Config.objects.get(conf_key="platform"))
+        self.driver= MobileAction(suite_id).driver()
+        self.element = MobileAction(suite_id).element(self.driver)
         self.start(**kwargs)
 
     def start(self, **kwargs):
@@ -27,7 +27,7 @@ class Login(object):
         try:
             account = kwargs.get("account")
             pwd = kwargs.get("pwd")
-            if "Android" == self.platform_name:
+            if "Android" == self.platform:
                 self.element.click(B.hwmconf_welcome_login_btn, "登录/注册")
                 self.element.click(B.login_choice_title, "密码登录")
                 self.element.input(B.hwmconf_login_account_input, account, "登录账户")
@@ -37,12 +37,16 @@ class Login(object):
                     self.element.click(B.hwmconf_dialog_button_right, "确定")
                 if self.element.is_display(B.base_dialog_title, "来电提醒设置"):
                     self.element.click(B.hwmconf_dialog_button_left, "跳过")
-            elif "iOS" == self.platform_name:
-                self.element.click(I.hwmconf_welcome_login_btn, "登录/注册")
-                self.element.click(I.login_choice_title, "密码登录")
-                self.element.input(I.hwmconf_login_account_input, account, "登录账户")
-                self.element.input(I.hwmconf_login_password_input, pwd, "登录密码")
-                self.element.click(I.hwmconf_login_login_btn, "登录")
+            elif "iOS" == self.platform:
+                self.element.click(I.login_btn, "登录/注册")
+                self.element.click(I.password_login, "密码登录")
+                if self.element.is_display(I.account_clean, "清空按钮"):
+                    self.element.click(I.account_clean, "清空")
+                self.element.input(I.account_input, account, "登录账户")
+                if self.element.is_display(I.password_clean, "清空按钮"):
+                    self.element.click(I.password_clean, "清空")
+                self.element.input(I.password_input, pwd, "登录密码")
+                self.element.click(I.login_btn, "登录")
                 if self.element.is_display(I.privacy_dialog_title, "隐私协议"):
                     self.element.click(I.hwmconf_dialog_button_right, "确定")
                 if self.element.is_display(I.base_dialog_title, "来电提醒设置"):
@@ -50,9 +54,11 @@ class Login(object):
             ExceptionAction().log().info("======执行" + self.__class__.__name__ + "结束")
         except Exception as e:
             ExceptionAction().log().error(e)
-            ExceptionAction().app_catch_image(self.driver)
+            ExceptionAction().app_catch_image(self.driver,suite_id)
             raise Exception(e)
 
 
-def get_plugin_class():
+def get_plugin_class(suite):
+    global suite_id
+    suite_id=suite
     return Login
