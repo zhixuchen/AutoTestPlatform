2d029ca7c32f7acc0da05696b50b6530654378ef
陈测试 <1195733026@qq.com>
2020-11-16 14:41:56 +0800

    Update __init__.py

--- global_variable/__init__.py
+++ global_variable/__init__.py
@@ -18,12 +18,18 @@ global REPORT_HTML_PATH
 global CONFIG_PATH
 global EXCEPTION_LOG_PATH
 global MOBILE_ACTION_PLUGIN_PATH
-
+global DRIVER
+global SUITE_PARAMS
 GLOBAL_VARIABLE_PATH = os.path.realpath(os.path.dirname(__file__))
 STATIC_FILE_PATH = os.path.join(os.path.dirname(GLOBAL_VARIABLE_PATH), 'static_file')
-EXCEPTION_IMAGE_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'report\\image\\')
-EXCEPTION_LOG_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'report\\log\\')
-REPORT_HTML_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'report\\html\\')
-CONFIG_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'config\\config.ini')
+EXCEPTION_IMAGE_PATH = os.path.join(os.path.realpath(GLOBAL_VARIABLE_PATH), '..\\..\\templates\\report\\image\\')
+EXCEPTION_LOG_PATH = os.path.join(os.path.realpath(GLOBAL_VARIABLE_PATH), '..\\..\\templates\\report\\log\\')
+REPORT_HTML_PATH = os.path.join(os.path.realpath(GLOBAL_VARIABLE_PATH), '..\\..\\templates\\report\\html\\')
+CONFIG_PATH = os.path.join(os.path.realpath(GLOBAL_VARIABLE_PATH), '..\\..\\templates\\config\\config.ini')
 CHROME_DRIVER_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'driver\\chromedriver.exe')
-MOBILE_ACTION_PLUGIN_PATH=os.path.join(os.path.realpath(STATIC_FILE_PATH), 'mobile_plugins')
+MOBILE_ACTION_PLUGIN_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'mobile_plugins')
+MOBILE_CASE_PLUGIN_PATH = os.path.join(os.path.realpath(STATIC_FILE_PATH), 'mobile_cases')
+DRIVER = None
+SUITE_PARAMS=None
+if __name__ == '__main__':
+    print(MOBILE_CASE_PLUGIN_PATH)
