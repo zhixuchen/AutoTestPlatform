bc539eaa7cbbbcd4a32e911ade41f826f7490d85
陈测试 <1195733026@qq.com>
2020-11-16 14:46:15 +0800

    Update suite.py

--- test_suite/mobile/suite.py
+++ test_suite/mobile/suite.py
@@ -8,30 +8,37 @@
 @File : suite.py
 @Project : AutoTestPlatform
 """
-# from test_case.web import login_case
-from test_case.moblie import login_case
-from function.report_html import *
-from function.send_email import SendEmail
+
+from WebforAutoTestPlatform.function.report_html import *
 import unittest
 
-
+import json
 class Suite(object):
     def __init__(self):
         self.suite = unittest.TestSuite()
 
-    def mobile_suite(self):
-        self.suite.addTest(login_case.LoginCase('login_case'))
-        # self.suite.addTest(login_case.LoginCase('login_out'))
+    def add_test_case(self, cases_name):
+        for case_name in cases_name:
+            case = __import__("WebforAutoTestPlatform.static_file.mobile_cases." + case_name,
+                              fromlist=[case_name])
+            case_class = case.get_case_class(SUITE_ID,SUITE_PARAMS)
+            self.suite.addTest(case_class(case_name))
         return self.suite
 
 
-if __name__ == '__main__':
-    suite_tests = Suite()
-    mobile_suite = suite_tests.mobile_suite()
-    report_name = "测试报告"
+def suite_test(cases_name, suite_id,suite_param):
+    global SUITE_PARAMS,SUITE_ID
+    SUITE_ID=suite_id
+    SUITE_PARAMS= json.loads(suite_param)
+    mobile_suite = Suite().add_test_case(cases_name)
+    report_name = "TestReport"
     description = "登录验证"
     report_html = ReportHtml()
-    html_file_path = report_html.build_report(mobile_suite, report_name, description)
+    html_file_path = report_html.build_report(mobile_suite, report_name,suite_id, description)
+    return html_file_path
 
-    # sent_email = SendEmail()
-    # sent_email.sent_email(html_file_path)
+
+if __name__ == '__main__':
+    case_name = ["LoginCase", "LoginOutCase"]
+    account = ""
+    # suite_test(case_name,**kwargs)
